215f83a8e12c98cbb57e0fd732d4ecdc6c7fda23
Radoslav98 <radiesto7@abv.bg>
2016-09-23 11:58:19 +0300

    Repair Program.cs

--- Fibbonaci/Program.cs
+++ Fibbonaci/Program.cs
@@ -9,12 +9,6 @@
             var f1= 1;
 			
 			//21312312
-            for (int i = 0; i < n-1; i++)
-			{
-			 var sum = f0 + f1;
-                f0=f1;
-                f1= sum;
-
 			}
             Console.WriteLine(f1);
 			//we dem boyza
